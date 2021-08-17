c698e085016f9af63069793243a287452a01bce8
dmiyamotoCAP <88892737+dmiyamotoCAP@users.noreply.github.com>
2021-08-17 10:15:26 -0400

    Added methods in Infra
    Just need implementation in homecontroller

--- Infra/IEmployeeDataAccess.cs
+++ Infra/IEmployeeDataAccess.cs
@@ -5,6 +5,11 @@ namespace InternalProject1.Infra{
     public interface IEmployeeDataAccess{
         List<Employee> GetAllEmployees();
         void SaveEmployee(Employee emp);
+
+        Employee GetEmployeeById(int id);
+
+        void UpdateEmployee(Employee department);
+        void DeleteEmployee(int id);
     }
 
 }
